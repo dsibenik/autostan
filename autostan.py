d74d74b472b65aaad082c88741bce1549c390256
Davor Sibenik <dsibenik@live.com>
2017-11-06 00:20:46 +0100

    updated

--- autostan.py
+++ autostan.py
@@ -12,37 +12,41 @@ import time
 import datetime
 import os
 
-
+mail_to = ["m.zanetic@hotmail.com", "dsibenik@live.com", "mzanetic@erstebank.com"]
 def send_mail(to, subject, text, attach):
-   msg = MIMEMultipart()
+    msg = MIMEMultipart()
 
-   msg['From'] = gmail_user
-   msg['To'] = to
-   msg['Subject'] = subject
+    msg['From'] = gmail_user
 
-   msg.attach(MIMEText(text))
+    if(type(to) is list):
+       msg['To'] = " ,".join(to)
+    else:
+       msg['To'] = to
+    msg['Subject'] = subject
 
-   #part = MIMEBase('application', 'octet-stream')
-   #part.set_payload(open(attach, 'rb').read())
-   #Encoders.encode_base64(part)
-   #part.add_header('Content-Disposition',
-   #        'attachment; filename="%s"' % os.path.basename(attach))
-   #msg.attach(part)
+    msg.attach(MIMEText(text))
 
-   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
-   mailServer.ehlo()
-   mailServer.starttls()
-   mailServer.ehlo()
-   mailServer.login(gmail_user, gmail_pwd)
-   mailServer.sendmail(gmail_user, to, msg.as_string())
-   # Should be mailServer.quit(), but that crashes...
-   mailServer.close()
+    #part = MIMEBase('application', 'octet-stream')
+    #part.set_payload(open(attach, 'rb').read())
+    #Encoders.encode_base64(part)
+    #part.add_header('Content-Disposition',
+    #        'attachment; filename="%s"' % os.path.basename(attach))
+    #msg.attach(part)
+
+    mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(gmail_user, gmail_pwd)
+    mailServer.sendmail(gmail_user, to, msg.as_string())
+    # Should be mailServer.quit(), but that crashes...
+    mailServer.close()
 
 
 print("Enter gmail account:")
 gmail_user = input()
 gmail_pwd = getpass()
-send_mail("dsibenik@live.com", "autostan started", "", None)
+send_mail(mail_to, "autostan started", "", None)
 
 njuskalo = "http://www.njuskalo.hr/iznajmljivanje-stanova?locationId=1153&price%5Bmin%5D=220&price%5Bmax%5D=405&mainArea%5Bmin%5D=29&mainArea%5Bmax%5D=65"
 index = "http://www.index.hr/oglasi/najam-stanova/gid/3279?pojam=&sortby=1&elementsNum=10&cijenaod=220&cijenado=405&tipoglasa=1&pojamZup=1153&grad=&naselje=&attr_Int_988=30&attr_Int_887=65&attr_bit_stan=&attr_bit_brojEtaza=&attr_gr_93_1=&attr_gr_93_2=&attr_Int_978=&attr_Int_1334=&attr_bit_prodavac=&attr_bit_eneregetskiCertifikat=&vezani_na=988-887_562-563_978-1334"
@@ -52,6 +56,7 @@ xpath_njuskalo = '//*[@id="form_browse_detailed_search"]/div/div[1]/div[3]/div[4
 xpath_index = "//div[@class='results']//a[@class='result']"
 xpath_oglasnik = '//div[@id="ads-list"]//a'
 
+os.system('clear')
 print("Initializing..")
 
 display = Xvfb()
@@ -60,17 +65,17 @@ driver = webdriver.Chrome()
 
 driver.get(njuskalo)
 elem = driver.find_elements_by_xpath(xpath_njuskalo)
-links = [e.get_attribute("href") for e in elem]
+links_njuskalo = [e.get_attribute("href") for e in elem]
 print("Got njuskalo..")
 
 driver.get(index)
 elem = driver.find_elements_by_xpath(xpath_index)
-links = links + [e.get_attribute("href") for e in elem]
+links_index = [e.get_attribute("href") for e in elem]
 print("Got index..")
 
 driver.get(oglasnik)
 elem = driver.find_elements_by_xpath(xpath_oglasnik)
-links = links + [e.get_attribute("href") for e in elem]
+links_oglasnik = [e.get_attribute("href") for e in elem]
 print("Got oglasnik..")
 
 print("Initialized!")
@@ -79,16 +84,18 @@ counter = 0
 try:
     while True:
         mail_body = ""
-        temp = []
+        temp_index = []
+        temp_njuskalo = []
+        temp_oglasnik = []
 
-        os.system('clear')
+        #os.system('clear')
         print(str(datetime.datetime.now()))
 
         ####### NJUSKALO
         try:
             driver.get(njuskalo)
             elem = driver.find_elements_by_xpath(xpath_njuskalo)
-            temp = temp + [e.get_attribute("href") for e in elem]
+            temp_njuskalo = [e.get_attribute("href") for e in elem]
             print("Got njuskalo..")
         except KeyboardInterrupt:
             driver.quit()
@@ -97,11 +104,11 @@ try:
             print("Skipping njuskalo!")
             pass
 
-        ########## INDEX
+        ####### INDEX
         try:
             driver.get(index)
             elem = driver.find_elements_by_xpath(xpath_index)
-            temp = temp + [e.get_attribute("href") for e in elem]
+            temp_index = [e.get_attribute("href") for e in elem]
             print("Got index..")
         except KeyboardInterrupt:
             driver.quit()
@@ -110,11 +117,11 @@ try:
             print("Skipping index!")
             pass
 
-        ########## OGLASNIK
+        ####### OGLASNIK
         try:
             driver.get(oglasnik)
             elem = driver.find_elements_by_xpath(xpath_oglasnik)
-            temp = temp + [e.get_attribute("href") for e in elem]
+            temp_oglasnik = [e.get_attribute("href") for e in elem]
             print("Got oglasnik..")
         except KeyboardInterrupt:
             driver.quit()
@@ -123,24 +130,41 @@ try:
             print("Skipping oglasnik!")
             pass
 
-        print()
-        for t in temp:
-            print(t)
-            if t not in links:
+        for t in temp_index+temp_njuskalo+temp_oglasnik:
+            if t not in links_index+links_njuskalo+links_oglasnik:
                 mail_body = mail_body + t + "\n"
 
         if mail_body != "":
             print("###############################")
             print("New entries found! Notifying..")
             print(mail_body)
-            links = temp
+
+            if len(temp_njuskalo) > 0:
+                links_oglasnik = temp_oglasnik
+
+            if len(temp_index) > 0:
+                links_index = temp_index
+
+            if len(temp_oglasnik) > 0:
+                links_njuskalo = temp_njuskalo
+
             counter += 1
             mail_title = "Novi stanovi - " + str(counter)
-            send_mail("dsibenik@live.com", mail_title, mail_body, None)
-            send_mail("m.zanetic@hotmail.com", mail_title, mail_body, None)
-            send_mail("mzanetic@erstebank.com", mail_title, mail_body, None)
 
-        time.sleep(30)
+            try:
+                send_mail(mail_to, mail_title, mail_body, None)
+            except Exception as e:
+                print(e + "\n" + mail_body)
+                send_mail("dsibenik@live.com", "Autostan error", e + "\n" + mail_body, None)
+                continue
+
+        else:
+            print("No new values found.")
+
+
+        print("Sleeping..........................")
+        time.sleep(5*30)
+        print()
 
 except KeyboardInterrupt:
     driver.quit()
