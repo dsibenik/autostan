5b7ef92a2db8eca2db4b38048f34d291754bde42
Davor <dsibenik@live.com>
2018-03-18 17:04:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# autostan
+A node program to scrape njuskalo.hr, index.hr/oglasi and oglasnik.hr for new entries.
+
+## Requirements
+[nodejs](https://nodejs.org/en/)
+
+## Installation
+1. Clone the node-dev branch of the repository:
+	```
+	git clone -b node-dev https://github.com/dsibenik/autostan
+	```
+
+2. Configure parameters in config.json:
+	- **from**: **gmail** address which sends notifications
+	- **to**: email address which receives notifications (can be the same as gmail)
+	- **subject**: subject line of the notifications
+	- **njuskalo**, **oglasnik**, **index**: links to respective sites with filters applied
+
+	Note: you might have to allow the app use the gmail account, instructions [here](https://support.google.com/accounts/answer/6010255?hl=en).
+
+3. Install node_modules:
+	```
+	npm install
+	```
+
+4. Start program:
+	```
+	npm start
+	```
---
+++ app.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const request = require("request-promise");
+const fs      = require('fs');
+
+const cheerio    = require("cheerio");
+const randomUA   = require('random-ua');
+const config     = require("./config.json");
+const nodemailer = require("nodemailer");
+const prompt     = require('password-prompt');
+const sleep      = require('sleep');
+
+
+const njuskalo = {
+	link: config.njuskalo,
+	uri: "www.njuskalo.hr"
+};
+const oglasnik = {
+	link: config.oglasnik,
+	uri: "www.oglasnik.hr"
+};
+const index = {
+	link: config.index,
+	uri: "www.index.hr"
+};
+
+const userAgent = randomUA.generate();
+
+async function getListings(site) {
+	let list = [];
+
+	if(site.link) {
+		const options = {
+						    url: site.link,
+						    headers: {
+								"Accept-Encoding" : "identity",
+								"User-Agent"      : userAgent
+						    },
+					  	};
+		try {
+			await request.get(options, function(error, response, html){
+			    if(!error){
+			        let $ = cheerio.load(html);
+
+			        // fs.writeFile("./response.html", html)
+
+			        if (site.uri == "www.njuskalo.hr")
+				        $('li.EntityList-item--Regular .entity-title a').each(function(){
+				    		list.push(this.attribs.href);
+				        });
+
+				    if (site.uri == "www.oglasnik.hr")
+				    	$('#ads-list a').each(function(){
+				    		list.push(this.attribs.href);
+				    	});
+
+				    if (site.uri == "www.index.hr")
+				    	$('div.results a.result').each(function(){
+				    		list.push(this.attribs.href)
+				    	});
+
+			    } else {
+			    	console.log(error);
+			    };
+			});
+		}
+		catch(error) {
+			console.log(error)
+		}
+	};
+
+	if (list.length == 0)
+		console.log("Error fetching", site.uri);
+
+	return list;
+}; 
+
+function mailNotification(content) {
+	console.log("Notifying...");
+	return;
+}
+
+async function main() {
+	const password = await prompt('Enter password for ' + config.from + ": ");
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+		    user: config.from,
+		    pass: password
+		}
+	});
+
+	let mailOptions = {
+		from: config.from, // sender address
+		to: config.to, // list of receivers
+		subject: config.subject, // Subject line
+		html: '<p>Program successfully started.</p>'// plain text body
+	};
+
+	transporter.sendMail(mailOptions, function (err, info) {
+	   if(err){
+	     console.log(err);
+	     process.exit();
+	   }
+	});
+
+	console.log("Initializing...");
+	let listingsOld = [].concat(await getListings(njuskalo), await getListings(index), await getListings(oglasnik));
+	console.log("Listings initialized!");
+
+	let counter = 0;
+	while(true) {
+		console.log("\nRefreshing @", new Date().toLocaleTimeString());
+		const listingsNew = [].concat(await getListings(njuskalo), await getListings(index), await getListings(oglasnik));
+		const diff = listingsNew.filter( x => !listingsOld.includes(x));
+
+		if(diff.length > 0) {
+			console.log("New listings: ", diff);
+
+			let html = "";
+			counter++;
+			for( let i; i < diff.length; i++)
+				html = html + "<p>" + diff[i] + "</p>";
+
+			mail.options.subject = config.subject + " " + counter;
+			mailOptions.html = html;
+
+			transporter.sendMail(mailOptions, function (err, info) {
+			   if(err)
+			     console.log(err);
+			});
+
+			listingsOld = listingsNew;
+		}
+		else {
+			console.log("No new listings.");
+		}
+		sleep.sleep(60*5);
+	};
+};
+
+main();
